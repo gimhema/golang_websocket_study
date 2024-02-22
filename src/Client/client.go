2cc4b3b0f5d94c6359afa8cce36a85c2fc59c9e6
gimhema <49871739+gimhema@users.noreply.github.com>
2024-02-22 11:57:05 +0900

    .
    .

--- src/Client/client.go
+++ src/Client/client.go
@@ -48,41 +48,15 @@ func main() {
 		}
 	}()
 
-	//ticker := time.NewTicker(time.Second)
-	//defer ticker.Stop()
-
 	for {
 		var message = ""
 		fmt.Print("Write Message : ")
 		fmt.Scanln(&message)
 
-		// send_message := prompt.WriteMessage()
-		c.WriteMessage(websocket.TextMessage, []byte(message))
-		// select {
-		// case <-done:
-		// 	return
-		// case t := <-ticker.C:
-		// 	err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-		// 	log.Printf("write message: %s", websocket.TextMessage)
-		// 	if err != nil {
-		// 		log.Println("write:", err)
-		// 		return
-		// 	}
-		// case <-interrupt:
-		// 	log.Println("interrupt")
+		if message == "exit" {
+			break
+		}
 
-		// 	// Cleanly close the connection by sending a close message and then
-		// 	// waiting (with timeout) for the server to close the connection.
-		// 	err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-		// 	if err != nil {
-		// 		log.Println("write close:", err)
-		// 		return
-		// 	}
-		// 	select {
-		// 	case <-done:
-		// 	case <-time.After(time.Second):
-		// 	}
-		// 	return
-		// }
+		c.WriteMessage(websocket.TextMessage, []byte(message))
 	}
 }
